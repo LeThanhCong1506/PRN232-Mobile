080bfa8719b76ee01bc84022f2b3a0a02aa0eb4a
Nguyễn Viết Hoàng <hoangnv10052004@gmail.com>
2026-04-01 10:42:59 +0700

    Hoang has done task.
    Refactor Payment

--- app/src/main/java/com/example/scamazon_frontend/data/network/api/PaymentApi.kt
+++ app/src/main/java/com/example/scamazon_frontend/data/network/api/PaymentApi.kt
@@ -4,9 +4,13 @@ import com.example.scamazon_frontend.data.models.common.BackendApiResponse
 import com.example.scamazon_frontend.data.models.payment.PaymentStatusResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface PaymentApi {
     @GET("payment/{orderId}/status")
     suspend fun getPaymentStatus(@Path("orderId") orderId: Int): Response<BackendApiResponse<PaymentStatusResponse>>
+
+    @PUT("payment/{orderId}/verify")
+    suspend fun verifyPaymentManually(@Path("orderId") orderId: Int): Response<BackendApiResponse<Any>>
 }
